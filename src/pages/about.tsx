c9b7ac4b8e8b29aac72d04f7e388ba3b861e4f7a
Baruch-Adi Hen <baruchadi@gmail.com>
2019-05-15 11:36:13 -0400

    basic about us user cards

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,12 +3,16 @@ import { graphql } from "gatsby"
 
 import SEO from "../components/seo"
 import { InfoSection } from "../components/molecules/info-section/info-section"
-import { BasicLayout } from "../layouts/basic-layout/basic-layout";
+import { BasicLayout } from "../layouts/basic-layout/basic-layout"
+import { UserCard } from "../components/molecules/user-card/user-card"
 
 const AboutUsPage = ({ data }) => (
   <BasicLayout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <InfoSection {...data.contentfulInfoBlock} />
+    {data.allContentfulPerson.nodes.map((node, i) => (
+      <UserCard {...node} num={i} key={node.id} />
+    ))}
   </BasicLayout>
 )
 
@@ -19,6 +23,26 @@ export const query = graphql`
       title
       body {
         json
+      }
+    }
+    allContentfulPerson {
+      nodes {
+        id
+        name
+        title
+        shortBio {
+          shortBio
+        }
+        image {
+          fluid(maxWidth: 150) {
+            ...GatsbyContentfulFluid_tracedSVG
+          }
+          file {
+            url
+          }
+        }
+        twitter
+        github
       }
     }
   }
